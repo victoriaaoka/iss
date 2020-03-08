be47a03b92f9c1ada0e847885048e87f468d6d0b
Victoria <victoriaaoka@gmail.com>
2020-03-08 22:02:50 +0300

    Fetch data from Open notify using HttpClient

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  constructor( private mapHttp: HttpClient) { }
+
+  getMapCoordinates(){
+    return this.mapHttp.get('http://api.open-notify.org/iss-now.json');
+  }
+
 }
+
